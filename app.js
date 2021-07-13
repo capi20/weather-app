9285c23e0ae161640370b7d3b29c56dccd8da872
capi20 <shyam20998@gmail.com>
2021-07-13 15:03:10 +0530

    third commit

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const axios = require('axios')
 const path = require('path')
 
 const geocode = require('./utils/geocode')
--- public/script.js
+++ public/script.js
@@ -4,6 +4,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+let currentUrl = ""
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -13,7 +14,9 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    currentUrl = window.location.href
+
+    fetch(`${currentUrl}weather?address=${location}`).then((response) => {
     response.json().then((data) => {
         if(data.error){
             messageOne.textContent = data.error
